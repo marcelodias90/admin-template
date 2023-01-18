be1b742c4452f4dd13e10d92ea8e1b5a23696862
Marcelo Dias <diasteixeira1990@gmail.com>
2023-01-18 08:17:35 -0300

    criado componentes e add autenticação com firebase

--- src/pages/notificacoes.tsx
+++ src/pages/notificacoes.tsx
@@ -1,9 +1,13 @@
 import Layout from "../components/template/Layout";
 
+
 export default function Notificacoes() {
+  
+
   return (
     <Layout titulo="Novidades" subtitulo="Aqui você irá gerenciar as sua novidades">
-        <h3>Conteúdo!!!!</h3>
+        
+        
     </Layout>
   )
 }
